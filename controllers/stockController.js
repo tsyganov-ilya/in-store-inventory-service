a3e8dd62369d0f2c9350d54ccb3905603600a613
Илья <tsyganov.ilya.d@gmail.com>
2025-02-26 16:01:28 +0300

    Added endpoint for increasing stock

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,5 +1,6 @@
 const pool = require('../config/dbConfig');
 
+// Создание остатка
 const createStock = async (req, res) => {
 
     const { product_id, shop_id, quantity_shelf, quantity_order } = req.body;
@@ -22,7 +23,35 @@ const createStock = async (req, res) => {
     }
 };  
 
-module.exports = { createStock };
+// Увеличение остатка улучшенный вариант
+const increaseStock = async (req, res) => {
+    const { id, amount } = req.body;
+
+    if (typeof amount !== 'number' || amount <= 0) {
+        return res.status(400).json({ error: 'Amount must be a positive number' });
+    }
+
+    try {
+        const result = await pool.query(
+            'UPDATE stock SET quantity_shelf = quantity_shelf + $1 WHERE id = $2 RETURNING *',
+            [amount, id]
+        );
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: 'Inventory item not found' });
+        }
+
+        res.json({
+            message: 'Inventory updated successfully',
+            updatedInventory: result.rows[0],
+        });
+    } catch (err) {
+        console.error('Error updating inventory:', err.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+module.exports = { createStock, increaseStock };
 
 
 
--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const { createStock } = require('../controllers/stockController.js');
+const { createStock, increaseStock } = require('../controllers/stockController.js');
 
 const router = express.Router();
 
-router.post('/', createStock);
+router.post('/', createStock); //POST http://localhost:3000/api/stocks
+// {
+//     "product_id": 1,
+//     "shop_id": 1,
+//     "quantity_shelf": 50,
+//     "quantity_order": 10
+// }
+router.patch('/', increaseStock); //PATCH http://localhost:3000/api/stocks/
+// {
+//     "id": 3,
+//     "amount": 10
+//   }
+  
 
 module.exports = router; 
